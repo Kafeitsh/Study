07082c0d322dd459b5b41d9a112fb7ba750b13b0
Kafeitsh <droozhbavitsh@yandex.ru>
2020-05-26 23:24:46 +0300

    duplicates deleted

--- Company&Employee/src/People/Worker.java
+++
@@ -1,9 +0,0 @@
-package People;
-
-public abstract class Worker implements Employee {
-    /**
-     * Переменные, используемые для пакета People
-     */
-    protected int monthSalary;
-    protected int employeeReturn;
-}
